4d490665b8b69d0e4e6e512e0e0db51fa462b1c9
Om22220074 <omkar.22220074@viit.ac.in>
2024-10-25 11:04:16 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+# Script to calculate simple interest
+
+echo "Enter Principal amount: "
+read p
+echo "Enter Rate of interest: "
+read r
+echo "Enter Time (in years): "
+read t
+
+si=$(echo "scale=2; $p * $r * $t / 100" | bc)
+echo "Simple Interest is: $si"
 
